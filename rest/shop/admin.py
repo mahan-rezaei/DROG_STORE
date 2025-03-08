46c851ad2da8fd00ac81ffed71ea4120fd02d48b
mahan-rezaei <mahanrezaei859@gmail.com>
2025-03-08 15:32:46 +0330

    add meta class to product model and create model admins"

--- rest/shop/admin.py
+++ rest/shop/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'is_sub', 'parent', 'created_at')
+    search_fields = ('name',)
+    list_filter  = ('is_sub',)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'stock', 'updated_at')
+    search_fields = ('name',)
