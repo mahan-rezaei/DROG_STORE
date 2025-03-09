429e8920e782224ab60e38ca541e7b3679f26361
mahan-rezaei <mahanrezaei859@gmail.com>
2025-03-09 15:18:55 +0330

    add category field for product serializer and Requirements

--- rest/shop/serializer.py
+++ rest/shop/serializer.py
@@ -21,7 +21,20 @@ class SubCategorySerializer(serializers.ModelSerializer):
         fields = ('id', 'name')
 
 
+class ProductCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('id', 'name')
+
+
+
 class ProductSerializer(serializers.ModelSerializer):
+    category = serializers.SerializerMethodField()
     class Meta:
         model = Product
-        fields = ('id', 'name', 'image', 'price', 'stock')
+        fields = ('id', 'name', 'image', 'price', 'stock', 'category')
+
+    def get_category(self, obj):
+        cat = obj.category.filter(is_sub=False)
+        ser_data = ProductCategorySerializer(instance=cat, many=True).data
+        return ser_data
