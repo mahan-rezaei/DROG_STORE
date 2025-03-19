b912b9d259cfb22f2b754e27aaed71a36c290bf1
mahan-rezaei <mahanrezaei859@gmail.com>
2025-03-19 17:20:52 +0330

    create the cart and cart item model for cart app

--- rest/cart/models.py
+++ rest/cart/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from shop.models import Product
 
-# Create your models here.
+User = get_user_model()
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='cart')
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"cart for {self.user.username}"
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    added_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.cart} - {self.product} - {self.quantity}"
